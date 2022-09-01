8ceb05c6d5a7af2e35e2292f65ce3794ce500e3b
BryanGonz <bgon@ucla.edu>
2022-08-31 21:44:53 -0500

    before installing the sanity image url function imageurlbuilder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,31 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-export default function HOme() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
-        <link rel='icon' href='/faicon.ico' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
 
-      <div className='flex justify-between items-center
-      bg-yellow-400 border-y border-black py-10 lg:py-0'>
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif' >
             <span className='underline decoration-black
             decoration-4'>
               Medium
             </span>{' '}
-            Is a place to write, read and connect
+            is a place to write, read and connect
           </h1>
           <h2>
             It's easy and free to post your thinking on
@@ -30,13 +36,35 @@ export default function HOme() {
 
         <img 
           className='hidden md:inline-flex h-32 lg:h-full'
-          src='https://acountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png'
+          src='https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png'
           alt=''
         />
 
       </div>
-
+      {/* Posts  */}
     </div>
 
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+    name,
+    image
+  },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts= await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
